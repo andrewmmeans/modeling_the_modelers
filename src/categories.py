b4549b16d207f4590296f45b64ee2bd55225d849
Andrew Means <andrewmmeans@gmail.com>
2020-07-20 11:08:05 -0700

    create category class

---
+++ src/categories.py
@@ -0,0 +1,14 @@
+from cgtrader_scraper import CgtraderScraper
+from scraper import Scraper
+
+class CategoryScraper(Scraper):
+
+    def __init__(self, name):
+        self.name = name
+        self.url = name.lower()
+
+    def 
+
+    
+
+    
