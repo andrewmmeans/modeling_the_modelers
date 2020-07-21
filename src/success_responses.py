31ab4fb770384b40dd2539b21a2582f083a8f1fa
Andrew Means <andrewmmeans@gmail.com>
2020-07-21 12:53:30 -0700

    add parallel processing and bug fix

--- src/success_responses.py
+++ src/success_responses.py
@@ -19,12 +19,11 @@ def process_response(filename):
     with open(DATA_DIR+'cgtrader/'+filename, 'rb') as f:
         response = pickle.load(f)
         if is_success(response):
-            save_to_file(filename, response, DATA_DIR+'success')
+            save_to_file(filename, response, DATA_DIR+'success/')
 
 
 if __name__ == '__main__':
     response_files = os.listdir(DATA_DIR + 'cgtrader/')
     with Pool(8) as p:
         p.map(process_response, response_files)
-
-
+        
