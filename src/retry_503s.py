c9a337213f8af12c56b0fb1616172851a31f345f
Andrew Means <andrewmmeans@gmail.com>
2020-08-13 17:12:30 -0700

    refactor url loading

--- src/retry_503s.py
+++ src/retry_503s.py
@@ -1,27 +1,58 @@
+from multiprocessing import Pool
 import argparse
+import requests
 import pickle
+import os
 
 
-
-folder = '~/data/cgtrader'
+folder = '/home/void/github/gal/capstones/modeling_the_modelers/data/'
+outdir = '/home/void/data/success/'
 
 files = os.listdir(folder)
+success_files = os.listdir(outdir)
 
 urls_to_fetch_again = []
 
-def main(folder):
+
+def is_duplicate(url):
+    return url.split('/')[-1] in success_files
+
+
+def process_files(files):
     for i, file in enumerate(files):
-        with open('/home/void/data/cgtrader/'+file, 'rb') as f:
-            response = pickle.load(f)
-            if response.status_code != 200:
-                urls_to_fetch_again.append(response.url)
+        if file.endswith('model_urls'):
+            with open(folder+file, 'rb') as f:
+                urls = pickle.load(f)
+            for url in urls:
+                if not is_duplicate(url):
+                    try:
+                        response = requests.get(url)
+                        if response.status_code == 200:
+                            with open(outdir + url.split('/')[-1], 'wb') as f:
+                                pickle.dump(response, f)
+                        else:
+                            urls_to_fetch_again.append(response.url)
+                    except Exception as e:
+                        print(f'Exception {e}')
+
+                else:
+                    print(f'Already retrieved {url}')
+    # with open(f'/home/void/data/','wb') as f:
+    #     pickle.dump(urls_to_fetch_again, f)
+
+
+
+def main(folder):
+    process_files(files)
+
     
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Retry 503 requests')
-    parser.add_argument('ind', metavar='in', type=str)
-    parser.add_argument('outd', metavar='out', type=str)
-    parser.add_argument('replace', metavar='r', type=str)
-    args = parser.parse_args()
+    #parser = argparse.ArgumentParser(description='Retry 503 requests')
+    #parser.add_argument('ind', metavar='in', type=str)
+    #parser.add_argument('outd', metavar='out', type=str)
+    #parser.add_argument('replace', metavar='r', type=str)
+    #args = parser.parse_args()
+    main(folder)
 
